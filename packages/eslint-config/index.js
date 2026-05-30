3849ce7f28084966588b92e257dd0f773fea5b3e
Thunderbird <daipeng@natureselect.ai>
2026-05-30 13:25:17 +0800

    feat(eslint-config): add shared TS ESLint config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    files: ["**/*.test.ts"],
+    rules: {
+      "no-console": "off",
+    },
+  },
+);
